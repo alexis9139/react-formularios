8a6b126b9e2459726cbe5c9b2ad51b3e4c32c758
Medrano Alexis Bruno Baloy <alexismedrano9139@gmail.com>
2020-04-14 17:42:00 -0300

    Manejando ( Inputs No controlados ) con Refs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class InputNoControlado extends React.Component {
+  //creo las ref
+  nombre = React.createRef()
+  email = React.createRef()
+
+  manejador = () => {
+    //aqui adentro debo obtener los datos desde el elemento input
+    const nombre = this.nombre.current.value
+    const email = this.email.current.value
+
+    //si queremos utilizar esos datos en el componente padre
+    //pasamos la informacion a traves de un objeto ({nombre,email})
+    this.props.enviarAlPadre({ nombre, email })
+  }
+
+  render() {
+    return (
+      <div>
+        {/* enlazo las ref */}
+        <input type="text" ref={this.nombre} placeholder="Ingrese nombre" />
+        <input type="text" ref={this.email} placeholder="Ingrese email" />
+        <button onClick={this.manejador}>Enviar al padre</button>
+      </div>
+    )
+  }
+}
+
+class App extends React.Component {
+  //send recibira los datos al momento que se envian 
+  //es decir en este punto --> this.props.enviarAlPadre({ nombre, email })
+  send = (datos) => {
+    console.log(datos)
+    console.log(datos.nombre)
+    console.log(datos.email)
+  }
+  render() {
+    //enviarAlPadre es aqui un escuchador de metodo
+    //es decir escucha lo que le manda el hijo
+
+    return (
+      <div>
+        <InputNoControlado enviarAlPadre={this.send} />
+      </div>
+    )
+  }
 }
 
 export default App;
